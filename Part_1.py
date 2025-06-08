8010198d23f9a3af7e22e40607f9b91491177c85
vaibhav9933 <vaibhavrahane36@gmail.com>
2025-06-08 21:18:15 +0530

    Continue

---
+++ Part_1.py
@@ -0,0 +1,12 @@
+#print("Hello World")
+#print("My name is Vaibhav")
+#print("My age is 12.")
+#print(5 + 2)
+name = 'vaibhav'
+age = 56
+price = 55.55
+print("My name is :",name)
+print("My age is :", age)
+print(type(name))
+print(type(age))
+print(type(price))
