20fe8746262be42a03396d0fe111a115d4c324b2
vaibhav9933 <vaibhavrahane36@gmail.com>
2025-06-12 12:31:15 +0530

    Continue

--- Part_1.py
+++ Part_1.py
@@ -2,6 +2,7 @@
 #print("My name is Vaibhav")
 #print("My age is 12.")
 #print(5 + 2)
+"""
 name = 'vaibhav'
 age = 56
 price = 55.55
@@ -10,3 +11,88 @@ print("My age is :", age)
 print(type(name))
 print(type(age))
 print(type(price))
+"""
+"""
+a = 2
+b = 3
+sum = a + b
+print(sum)
+"""
+
+
+# Arithmatic operator
+"""
+a = 5
+b = 2
+print (a + b)
+print(a - b)
+print(a * b)
+print( a / b)
+print( a % b)
+print( a ** b)
+"""
+
+# Relational operator
+"""
+a = 50
+b = 20
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a > b)
+print(a<= b)
+print(a < b)
+"""
+
+# Assignment operator
+
+"""
+ = , += , -= , *= , /= , %= , **= 
+"""
+"""
+num = 10
+num = num + 10
+print("num : ",num)
+"""
+
+# Logical operator
+"""'
+not , and , or
+"""
+"""
+
+print(not False)
+print(not True)
+
+a = 50
+b = 30
+print(not(a > b))
+
+val1 = True
+val2 = True
+print("AND operator:",val1 and val2)
+print("OR operator:",val1 or val2)
+"""
+
+# Type Conversion
+"""
+a = int("2")
+b = 4.25
+
+sum = a + b
+print(type(a))
+print(sum)
+"""
+
+"""
+
+a = 3.14
+a = str(a)
+print(type(a))
+"""
+
+#To take input
+
+name = input("Enter you Name:")
+print("Welcome", name)
+print(type(name),name)
