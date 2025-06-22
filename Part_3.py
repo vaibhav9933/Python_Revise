30a8405ca8484296d817feb7383f34e469c09b41
vaibhav9933 <vaibhavrahane36@gmail.com>
2025-06-22 17:36:01 +0530

    Continue

--- Part_3.py
+++ Part_3.py
@@ -1,2 +1,10 @@
-marks = [94.4,87.5,15.5,14.5,35.5]
+#List
+"""
+marks = [94.4,87.5,15.5,14.5,35.5,"apple","hee"]
 print(marks)
+print(type(marks))
+print(marks[0])
+print(len(marks))
+marks[0]="Vaibhav"
+print(marks) """
+
