14f9a0615f3e9bd7e0abd25c79b50dbdd3eaf551
vaibhav9933 <vaibhavrahane36@gmail.com>
2025-06-24 09:56:01 +0530

    Continue

--- Part_3.py
+++ Part_3.py
@@ -7,4 +7,30 @@ print(marks[0])
 print(len(marks))
 marks[0]="Vaibhav"
 print(marks) """
+"""
+marks = [85,55,66,82,62,55]
+print(marks[1:4])
+
+"""
+#methods
+
+"""
+list = [2,1,6]
+#list.append(5)
+#list.sort()
+#list.sort(reverse=True)
+#list.insert(1,88)
+#list.remove(2)
+#list.pop(2)
+
+print(list)
+"""
+
+#Tuples in Python
+
+tup = (2,1,3,5,6,8)
+#print(type(tup))
+print(tup[0])
+
+
 
