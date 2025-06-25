a7402452c47dafb9e9395ec1d91286d39de6830b
vaibhav9933 <vaibhavrahane36@gmail.com>
2025-06-25 13:11:37 +0530

    Continue

--- Part_3.py
+++ Part_3.py
@@ -27,10 +27,49 @@ print(list)
 """
 
 #Tuples in Python
-
+"""
 tup = (2,1,3,5,6,8)
 #print(type(tup))
 print(tup[0])
+"""
 
+#methods
+"""
+tup = (2,3,51,4,88,6,5,7,2,2,2)
+print(tup.index(3))
+print(tup.count(2))
+"""
 
+# Let's Practice
+# wap to ask user to enter name of there 3 favorite movies and stores them in a list
+"""
+mov1 = input("Enter movie:")
+mov2 = input("Enter movie:")
+mov3 = input("Enter movie:")
 
+movies = []
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
+"""
+#wap to check if a list contains a palindrom of elements
+"""
+l1 = [1,2,3,2,1]
+l2 = [2,5,8,8,8]
+copy_l1 = l1.copy()
+copy_l1.reverse()
+
+if (copy_l1 == l1):
+    print("Palendrom")
+else:
+    print("Not a palendrom")
+"""
+
+#wap to count the number of studnets wiwth the "A" grade in the followiwng tuple
+# store the above values in a list and store them from A - D
+
+grade = ["C","D","A","A","A","B","B","A"]
+grade.sort()
+print(grade)
