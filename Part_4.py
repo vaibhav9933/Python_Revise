5cf5a767fb3132e62a0ccefa9355793d0de997d5
vaibhav9933 <vaibhavrahane36@gmail.com>
2025-07-08 20:13:39 +0530

    Continue

--- Part_4.py
+++ Part_4.py
@@ -1,1 +1,19 @@
 #set & dictionary
+info ={
+    "key": "Value",
+    "subject": ["python","c++","java","c"],
+    "name": "rahane",
+    "topics": ("dict","set"),
+    "gujarat":"pr",
+    "age": 56,
+    "is_age":True,
+    "marks": 52.55
+
+}
+#print(info)
+#print(info["topics"])
+#print(info["subject"])
+#print(info["age"])
+
+info["name"] = "vishal"
+print(info)
